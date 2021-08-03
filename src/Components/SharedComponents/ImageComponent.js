bd7d613fe75fa3fa47afa139cd3c1add681ada2a
kevinpaulJose <kevinjosephpaul.twa@gmail.com>
2021-08-03 13:44:52 +0530

    Images onClick

--- src/Components/SharedComponents/ImageComponent.js
+++ src/Components/SharedComponents/ImageComponent.js
@@ -6,7 +6,9 @@ export default function ImageComponent({ props }) {
     <div className="col-6 col-md-4 col-lg-3">
       <div className="d-block mb-4 h-100 ">
         <div className="img">
-          <img className="img-inner" src={props.href} alt="img" />
+          <a href={props.href} target="_blank" rel="noreferrer">
+            <img className="img-inner" src={props.href} alt="img" />
+          </a>
         </div>
       </div>
     </div>
